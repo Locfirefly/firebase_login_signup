775a1bd1013be7e7ab989b274ca551e738a25be3
Quang Locc <locfirefly2507@gmail.com>
2023-10-12 14:23:59 +0700

    add profile UI

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -9,6 +9,7 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  bool isSwitch = false;
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -33,6 +34,239 @@ class _ProfileState extends State<Profile> {
                   radius: 70,
                 ),
               ),
+              SizedBox(height: SizeConfig.screenHeight * 0.02),
+              const Center(
+                child: Text('Firefly Nguyen',
+                  style: TextStyle(
+                      fontSize: 19,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xfff77546),
+                  ),
+                ),
+              ),
+              Center(
+                child: Text('abvcd245633@gmail.com',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey.shade700
+                  ),
+                ),
+              ),
+              SizedBox(height: SizeConfig.screenHeight * 0.04),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        borderRadius: const BorderRadius.only(
+                            topRight: Radius.circular(20),
+                            topLeft: Radius.circular(20),
+                          ),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.edit,
+                                  size: 30,
+                                  color: Color(0xfff77546),
+                                ),
+                                SizedBox(width: 10),
+                                Text('Edit your profile',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Icon(Icons.arrow_forward_ios_outlined)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: SizeConfig.screenHeight * 0.003),
+                    Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.language_outlined,
+                                  size: 30,
+                                  color: Color(0xfff77546),
+                                ),
+                                SizedBox(width: 10),
+                                Text('Language',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Icon(Icons.arrow_forward_ios_outlined)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: SizeConfig.screenHeight * 0.003),
+                    Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        borderRadius: const BorderRadius.only(
+                          bottomRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(20),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Row(
+                              children: [
+                                Icon(Icons.dark_mode_rounded,
+                                  size: 30,
+                                  color: Color(0xfff77546),
+                                ),
+                                SizedBox(width: 10),
+                                Text('Dark mode',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Transform.scale(
+                              scale: 0.7,
+                              child: Switch(
+                                activeColor: const Color(0xfff77546),
+                                  value: isSwitch,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      isSwitch = value;
+                                    });
+                                  }
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: SizeConfig.screenHeight * 0.02),
+                    Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.info_rounded,
+                                  size: 30,
+                                  color: Color(0xfff77546),
+                                ),
+                                SizedBox(width: 10),
+                                Text('About',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Icon(Icons.arrow_forward_ios_outlined)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: SizeConfig.screenHeight * 0.02),
+                    Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.feedback_rounded,
+                                  size: 30,
+                                  color: Color(0xfff77546),
+                                ),
+                                SizedBox(width: 10),
+                                Text('Send feedback',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Icon(Icons.arrow_forward_ios_outlined)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: SizeConfig.screenHeight * 0.02),
+                    Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.logout_outlined,
+                                  size: 30,
+                                  color: Color(0xfff77546),
+                                ),
+                                SizedBox(width: 10),
+                                Text('LogOut',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Icon(Icons.arrow_forward_ios_outlined)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
